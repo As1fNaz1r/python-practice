620523e5a96092f78cac2741538479eaf95d97cc
Asif Nazir <asifnazirbhat313@gmail.com>
2026-02-21 14:42:01 +0530

    add class

---
+++ oop-basics/without_class.py
@@ -0,0 +1,11 @@
+# Without classes
+
+person1_name = 'Alice'
+person1_age = 25
+person1_email = 'alice@email.com'
+
+person2_name = 'Bob'
+person2_age = 30
+person2_email = "bob@email.com"
+
+# This gets messy fast with 100 people!
